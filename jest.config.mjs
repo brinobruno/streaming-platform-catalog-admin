61ca0ffc5aad8ad9739ae3e7440c24dbf7ff9fe5
Bruno Correa <brunosantos6ft@gmail.com>
2025-10-21 20:39:26 +0000

    feat(build): implement testing/ts setup

---
+++ jest.config.mjs
@@ -0,0 +1,26 @@
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+import { pathsToModuleNameMapper } from 'ts-jest';
+import { readFileSync } from 'fs';
+
+const tsconfig = JSON.parse(readFileSync('./tsconfig.json', 'utf8'));
+
+const config = {
+  preset: 'ts-jest/presets/default-esm',
+  extensionsToTreatAsEsm: ['.ts', '.tsx'],
+  moduleNameMapper: pathsToModuleNameMapper(tsconfig.compilerOptions.paths, { prefix: '<rootDir>/' }),
+  rootDir: '.',
+  clearMocks: true,
+  collectCoverage: true,
+  coverageProvider: "v8",
+  transform: {
+    '^.+\\.(t|j)sx?$': ['ts-jest', {
+      useESM: true
+    }],
+  },
+};
+
+export default config;
