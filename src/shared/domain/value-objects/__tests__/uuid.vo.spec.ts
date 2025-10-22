be6897faba9f40c072f5ff2ac191c55b73fb04a8
Bruno Correa <brunosantos6ft@gmail.com>
2025-10-22 01:18:39 +0000

    feat: add value object uuid

---
+++ src/shared/domain/value-objects/__tests__/uuid.vo.spec.ts
@@ -0,0 +1,31 @@
+import { InvalidUuidError, Uuid } from '@/shared/domain/value-objects/uuid.vo.ts'
+import { jest } from '@jest/globals'
+
+describe('Uuid unit tests', () => {
+  const validateSpy = jest.spyOn(Uuid.prototype as any, 'validate')
+
+  describe('constructor', () => {
+    it('should throw error if uuid is invalid', () => {
+      expect(() => new Uuid('invalid-uuid')).toThrow(InvalidUuidError)
+      expect(validateSpy).toHaveBeenCalledTimes(1)
+    })
+
+    it("should create a valid uuid", () => {
+      const uuid = new Uuid()
+      expect(uuid.id).toBeDefined()
+      expect(validateSpy).toHaveBeenCalledTimes(1)
+    })
+
+    it("should create a valid uuid when a valid uuid is provided", () => {
+      const validUuid = '123e4567-e89b-12d3-a456-426614174000'
+      const uuid = new Uuid(validUuid)
+      expect(uuid.id).toBe(validUuid)
+      expect(validateSpy).toHaveBeenCalledTimes(1)
+    })
+
+    it("should throw error when an invalid uuid is provided", () => {
+      expect(() => new Uuid('invalid-uuid')).toThrow(InvalidUuidError)
+      expect(validateSpy).toHaveBeenCalledTimes(1)
+    })
+  })
+})
